07bb0382aeb895b12b64da656fbadd836d254a0d
Roshan Singh <sidharthmishra648@gmail.com>
2023-05-27 15:22:14 +0530

    First commit

---
+++ Arrays/Medium/merge_nodes_in_between_zeroe.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        int sum=0;
+        ListNode* ptr=head; 
+        ListNode* ptr1=head;
+        while(ptr1->next!=NULL)
+        {
+            ptr=ptr->next;
+            if(ptr->val!=0)
+            {
+                sum=sum+ptr->val;
+            }
+            else
+            {
+                ptr->val=sum;
+                ptr1->next=ptr;
+                ptr1=ptr1->next;
+                sum=0;
+            }
+        }
+        head=head->next;
+        return head;
+    }
+};
