1a6f0c50037493c2810849b0d1bf9cf0f3e6c899
Roshan Singh <sidharthmishra648@gmail.com>
2023-05-27 17:11:18 +0530

    First commit

---
+++ Arrays/Medium/maximum_twin_sum_of_a_linked_list.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        vector<int> arr;
+        int count=1,ans=0;
+        ListNode* ptr=head;
+        arr.push_back(head->val);
+        while(ptr->next!=NULL)
+        {
+            ptr=ptr->next;
+            arr.push_back(ptr->val);
+            count++;
+        }    
+        for(int i=0;i<count/2;i++)
+        {
+            ans= (arr[i]+arr[count-i-1])>ans?arr[i]+arr[count-i-1]:ans;
+        }
+        return ans;
+    }
+};
