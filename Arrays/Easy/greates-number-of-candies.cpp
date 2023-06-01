c644fe664a92c6332510ec16cd7430bbacad0d44
Sidharth Mishra <106550602+SIDHARTH-7@users.noreply.github.com>
2023-06-01 23:47:48 +0530

    1 jun 23

---
+++ Arrays/Easy/greates-number-of-candies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int max=*max_element(candies.begin(), candies.end());
+        vector<bool> result;
+        for(int i=0;i<candies.size();i++)
+        {
+            if(extraCandies+candies[i]>=max)
+                result.push_back(true);
+            else
+                result.push_back(false);
+        }    
+        return result;
+    }
+};
